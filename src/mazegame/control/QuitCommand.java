0c2e205a04a97379c1e9e966a71c1a02e0572c9f
Chirag Chaplot <chirag.chaplot@bp.com>
2024-10-03 22:13:55 +1000

    Initial Commit - Lab 7 and Lab 8

---
+++ src/mazegame/control/QuitCommand.java
@@ -0,0 +1,10 @@
+package mazegame.control;
+
+import mazegame.entity.Player;
+
+public class QuitCommand implements Command {
+
+	public CommandResponse execute (ParsedInput input, Player thePlayer) {
+		return new CommandResponse ("Thanks for playing --- Goodbye", true);
+	}
+}
